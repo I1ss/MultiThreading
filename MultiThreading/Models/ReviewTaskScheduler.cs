65c7294f7937af98a605f79179e3c60f3d0213bb
I1ss <lebedewgeorg@yandex.ru>
2022-10-24 00:05:35 +0700

    Add second lr.

---
+++ MultiThreading/Models/ReviewTaskScheduler.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Threading;
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace MultiThreading.Models
+{
+    internal class ReviewTaskScheduler : TaskScheduler
+    {
+        private LinkedList<Task> _tasksList;
+
+        public ReviewTaskScheduler()
+        {
+            _tasksList = new LinkedList<Task>();
+        }
+
+        public ReviewTaskScheduler(LinkedList<Task> tasks)
+        {
+            _tasksList = tasks;
+        }
+
+        protected override IEnumerable<Task> GetScheduledTasks()
+        {
+            return _tasksList;
+        }
+
+        protected override void QueueTask(Task task)
+        {
+            Console.WriteLine($"[Queue Task] Задача #{task.Id} поставлена в очередь...");
+            if (_tasksList?.Count >= 10)
+                _tasksList = new LinkedList<Task>();
+
+            _tasksList.AddLast(task);
+            ThreadPool.QueueUserWorkItem(ExecuteTasks, null);
+            Thread.Sleep(2000);
+        }
+
+        protected override bool TryExecuteTaskInline(Task task, bool taskWasPreviouslyQueued)
+        {
+            Console.WriteLine($"[TryExecuteTaskInline] Попытка выполнить задачу #{task.Id} синхронно...");
+
+            lock (_tasksList)
+            {
+                _tasksList.Remove(task);
+            }
+
+            return base.TryExecuteTask(task);
+        }
+
+        protected override bool TryDequeue(Task task)
+        {
+            Console.WriteLine($"[TryDequeue] Попытка удалить задачу {task.Id} из очереди...");
+            var result = false;
+
+            lock (_tasksList)
+            {
+                result = _tasksList.Remove(task);
+            }
+
+            if (result == true)
+                Console.WriteLine($"[TryDequeue] Задача {task.Id} была удалена из очереди на выполнение...");
+
+            return result;
+        }
+
+        private void ExecuteTasks(object _)
+        {
+            while (true)
+            {
+                Task task;
+
+                lock (_tasksList)
+                {
+                    if (_tasksList.Count == 0)
+                        break;
+
+                    task = _tasksList.First.Value;
+                    _tasksList.RemoveFirst();
+                }
+
+                if (task == null)
+                    break;
+
+                base.TryExecuteTask(task);
+            }
+        }
+
+        /// <summary>
+        /// Нахождение задачи и перемещение её на последнее место в очереди.
+        /// </summary>
+        /// <param name="task"> Искомая задача. </param>
+        /// <returns> True - операция завершилась удачно, false - неудачно. </returns>
+        public bool SetTaskPositionToLast(Task task)
+        {
+            var neededTask = _tasksList?.FirstOrDefault(taskNum => taskNum.Id == task.Id);
+            if (neededTask is null)
+                return false;
+
+            lock (_tasksList)
+            {
+                _tasksList?.AddLast(neededTask);
+                _tasksList?.Remove(_tasksList?.FirstOrDefault(taskNum => taskNum.Id == task.Id));
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Нахождение задачи и перемещение её на первое место в очереди.
+        /// </summary>
+        /// <param name="task"> Искомая задача. </param>
+        /// <returns> True - операция завершилась удачно, false - неудачно. </returns>
+        public bool SetTaskPositionToFirst(Task task)
+        {
+            var neededTask = _tasksList?.FirstOrDefault(taskNum => taskNum.Id == task.Id);
+            if (neededTask is null)
+                return false;
+
+            lock (_tasksList)
+            {
+                _tasksList?.Remove(_tasksList?.FirstOrDefault(taskNum => taskNum.Id == task.Id));
+                _tasksList?.AddFirst(neededTask);
+            }
+
+            return true;
+        }
+    }
+}
--- MultiThreading/Program.cs
+++ MultiThreading/Program.cs
@@ -1,23 +1,83 @@
-﻿using System.Threading;
-
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MultiThreading.Models;
 using MultiThreading.MultiThreadingUtils;
 
 namespace MultiThreading
 {
     class Program
     {
+        private const string FIRST_LR = "FIRST";
+
+        private const string SECOND_LR = "SECOND";
+
         static void Main(string[] args)
         {
-            var threadFirst = MultiThreadingUtilities.StartWorkingThread();
+            switch (SECOND_LR)
+            {
+                case FIRST_LR:
+                    var threadFirst = MultiThreadingUtilities.StartWorkingThread();
 
-            Thread.Sleep(2000);
+                    Thread.Sleep(2000);
 
-            var taskFirst = MultiThreadingUtilities.StartWorkingTask();
+                    var taskFirst = MultiThreadingUtilities.StartWorkingTask();
 
-            Thread.Sleep(2000);
+                    Thread.Sleep(2000);
 
-            // Рассказать про Thread.Sleep().
-            var poolThreadFirst = MultiThreadingUtilities.StartWorkingPoolThread();
+                    // Рассказать про Thread.Sleep(). Передаёт поток после завершения, нарушает асинхронность.
+                    var poolThreadFirst = MultiThreadingUtilities.StartWorkingPoolThread();
+                    break;
+                case SECOND_LR:
+                    var tasks = new Task[10];
+                    var reviewTaskScheduler = new ReviewTaskScheduler();
+
+                    QueueTaskTesting(tasks, reviewTaskScheduler);
+                    Task.WaitAll(tasks);
+
+                    // 2nd part.
+
+                    var tasksList = new LinkedList<Task>();
+                    
+                    for (var i = 0; i < 10; i++)
+                    {
+                        tasks[i] = new Task(() =>
+                        {
+                            Thread.Sleep(2000);
+                            Console.WriteLine($"Задача {Task.CurrentId} выполнилась в потоке {Thread.CurrentThread.ManagedThreadId}.\n");
+                        });
+
+                        tasksList.AddLast(tasks[i]);
+                    }
+
+                    reviewTaskScheduler = new ReviewTaskScheduler(tasksList);
+                    reviewTaskScheduler.SetTaskPositionToLast(tasksList.FirstOrDefault(task => task.Id > 15));
+
+                    reviewTaskScheduler = new ReviewTaskScheduler(tasksList);
+                    reviewTaskScheduler.SetTaskPositionToFirst(tasksList.FirstOrDefault(task => task.Id > 12));
+
+                    QueueTaskTesting(tasksList.ToArray(), reviewTaskScheduler);
+                    Task.WaitAll(tasksList.ToArray());
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private static void QueueTaskTesting(Task[] tasks, ReviewTaskScheduler reviewTaskScheduler)
+        {
+            for (var i = 0; i < tasks.Length; i++)
+            {
+                tasks[i] = new Task(() =>
+                {
+                    Thread.Sleep(2000);
+                    Console.WriteLine($"Задача {Task.CurrentId} выполнилась в потоке {Thread.CurrentThread.ManagedThreadId}.\n");
+                });
+
+                tasks[i].Start(reviewTaskScheduler);
+            }
         }
     }
 }
